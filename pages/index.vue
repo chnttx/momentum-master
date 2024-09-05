011ebada38827127ac9f6d34f2337d63478aaea2
Anandi Nambiar <annambi@student.unimelb.edu.au>
2024-09-05 21:45:36 +1000

    padding changed

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<!-- Dashboard layout -->
+<template>
+
+    <div class="dashboard-layout">
+        
+         <Header />
+    
+         <Sidebar />    
+        
+    </div>
+</template>
+
+<script>
+import Header from '~/components/Header.vue'
+import Sidebar from '~/components/Sidebar.vue'
+
+export default {
+    components: {
+        Header,
+        Sidebar
+       
+    }
+}
+</script>
+
+<style scoped lang="scss">
+
+@import "~/assets/scss/_colors.scss";
+@import "~/assets/scss/_fonts.scss";
+.dashboard-layout {
+    background: linear-gradient(to bottom, white, #5085a5);
+
+}
+</style>
