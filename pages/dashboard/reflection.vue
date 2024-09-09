d1a9960bc2a2d3257955a17e36200cc871942098
cyy020811 <jimyang@gmail.com>
2024-09-09 15:18:44 +1000

    feat: user profile data used in header

---
+++ pages/dashboard/reflection.vue
@@ -0,0 +1,25 @@
+<template>
+    <NuxtLayout :name="layout">
+        <div>{{ session }}</div>
+    </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+interface Chat {
+    username: string;
+    text: string;
+    time: string;
+}
+const { getSession } = useAuth();
+
+const layout = "board";
+const message = ref("");
+const chats = ref<Chat[]>([]);
+const session = await getSession();
+
+const sendMessage = async () => {
+    /* To ChatGPT API */
+};
+</script>
+
+<style scoped lang="scss"></style>
