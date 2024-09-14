2267f236bd62347c49f004470c0ec61d1b3618b1
chnttx <tronghieun@student.unimelb.edu.au>
2024-09-14 13:04:25 +1000

    task(api): storing user skill rating + getting skill used

---
+++ server/services/skill-used.ts
@@ -0,0 +1,43 @@
+import prisma from "~/lib/prisma";
+
+//TODO: Write API endpoints to store user skill rating, 
+//TODO: retrieve information regarding skill used
+
+// Get skill used by user
+export const getSkillUsedByUser = async (userId: number) => { 
+    const allReflectionsByUser = await prisma.reflection.findMany({
+        where: {
+            user_id: userId
+        },
+        select: {
+            reflection_id: true
+        }
+    })
+
+    const ratingsByUser = await prisma.skill_used.findMany({
+        where: {
+            reflection_id: {
+                in: allReflectionsByUser.map(reflection => reflection.reflection_id)
+            }
+        },
+        select: {
+            id_rating: true,
+            id_skill: true,
+        }
+    })
+
+    return ratingsByUser    
+}
+
+
+export const storeUserSkillRating = async (reflection_id: number, rating_id: number, skill_id: number ) => {
+    const reflectionSkillRating = await prisma.skill_used.create({
+        data: {
+            reflection_id: reflection_id,
+            id_rating: rating_id,
+            id_skill: skill_id
+        }
+    })
+
+    return reflectionSkillRating
+}
