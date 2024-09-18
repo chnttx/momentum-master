d969d8339e833cc7a2fb1977cc0b37f5e9b1232d
cyy020811 <jimyang@gmail.com>
2024-09-18 11:21:48 +1000

    fix: add nuxt/ui module back to config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,21 +3,23 @@
 export default defineNuxtConfig({
     compatibilityDate: "2024-04-03",
     devtools: { enabled: true },
-    modules: ["@nuxt/test-utils/module", "@sidebase/nuxt-auth"],
+    modules: ["@nuxt/test-utils/module", "@sidebase/nuxt-auth", "@nuxt/ui"],
     runtimeConfig: {
         public: {
-            BASE_URL: process.env.BASE_URL || `https://${process.env.VERCEL_URL}`,
+            BASE_URL:
+                process.env.BASE_URL || `https://${process.env.VERCEL_URL}`,
             AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
-            AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID
-        }
-
+            AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+        },
     },
     auth: {
         globalAppMiddleware: true,
         isEnabled: true,
         disableServerSideAuth: false,
         originEnvKey: "AUTH_ORIGIN",
-        baseURL: process.env.BASE_URL || `https://${process.env.VERCEL_URL}/api/auth`,
+        baseURL:
+            process.env.BASE_URL ||
+            `https://${process.env.VERCEL_URL}/api/auth`,
         provider: {
             type: "authjs",
             trustHost: false,
