63d81f824e614e25c25c907192514a2e0ed511e5
chnttx <tronghieun@student.unimelb.edu.au>
2024-09-27 19:47:01 +1000

    added empty settings page

---
+++ pages/dashboard/settings.vue
@@ -0,0 +1,23 @@
+<template>
+    <NuxtLayout :name="layout">
+        <div class="user-settings">
+            <ul v-for="setting in settings">
+                <li>{{ setting.description }}</li>
+            </ul>
+        </div>  
+    </NuxtLayout>
+</template>
+
+
+<script setup lang="ts">
+const layout="board"
+const settings = [
+    {
+        description: "Enable email notifications",
+    },
+    {
+        description: "Enable dark mode"
+    }
+]
+// const 
+</script>
