c4e6c5b827539f60b630823a5e47dd08756a47a4
cyy020811 <jimyang@gmail.com>
2024-10-03 17:23:55 +1000

    fix(journal): change the styling of journal components

--- components/Summary.vue
+++ components/Summary.vue
@@ -1,62 +1,124 @@
-@import "~/assets/scss/_fonts.scss";
-@import "~/assets/scss/_colors.scss"
-
 <template>
-  <div class="page-container">
-    <div class="header">
-        <h1> Date: {{currentDate}}</h1>
-        <div class=" title">
-        <h2> Title </h2>
+    <div class="page-container">
+        <div class="header">
+            <h1>Date: {{ currentDate }}</h1>
+            <div class="title">
+                <h2>Title</h2>
+            </div>
         </div>
-      </div>
-  
-      <div class="content">
-        <div class="skills">
-        <p>Skill Used: <span class="skill-item">{{ skill }}</span></p>
-    </div>
-    <div class="summary">
-        <h1>Summary:</h1>
-        <p>{{ summary }}</p>
+
+        <div class="content">
+            <div class="skills">
+                <p>Skill Used:</p>
+                <span class="skill-item">{{ skill }}</span>
+            </div>
+            <div class="summary">
+                <h1>Summary:</h1>
+                <p>{{ summary }}</p>
+            </div>
+            <div class="dropdown">
+                <div @click="toggleDropdown" class="dropdown-toggle">
+                    History
+                </div>
+                <div v-if="isOpen" class="dropdown-content">
+                    <div class="scrollable-area">
+                        <p>
+                            Lorem Ipsum is simply dummy text of the printing and
+                            typesetting industry. Lorem Ipsum has been the
+                            industry's standard dummy text ever since the 1500s,
+                            when an unknown printer took a galley of type and
+                            scrambled it to make a type specimen book. It has
+                            survived not only five centuries, but also the leap
+                            into electronic typesetting, remaining essentially
+                            unchanged. It was popularised in the 1960s with the
+                            release of Letraset sheets containing Lorem Ipsum
+                            passages, and more recently with desktop publishing
+                            software like Aldus PageMaker including versions of
+                            Lorem Ipsum Lorem Ipsum is simply dummy text of the
+                            printing and typesetting industry. Lorem Ipsum has
+                            been the industry's standard dummy text ever since
+                            the 1500s, when an unknown printer took a galley of
+                            type and scrambled it to make a type specimen book.
+                            It has survived not only five centuries, but also
+                            the leap into electronic typesetting, remaining
+                            essentially unchanged. It was popularised in the
+                            1960s with the release of Letraset sheets containing
+                            Lorem Ipsum passages, and more recently with desktop
+                            publishing software like Aldus PageMaker including
+                            versions of Lorem Ipsum Lorem Ipsum is simply dummy
+                            text of the printing and typesetting industry. Lorem
+                            Ipsum has been the industry's standard dummy text
+                            ever since the 1500s, when an unknown printer took a
+                            galley of type and scrambled it to make a type
+                            specimen book. It has survived not only five
+                            centuries, but also the leap into electronic
+                            typesetting, remaining essentially unchanged. It was
+                            popularised in the 1960s with the release of
+                            Letraset sheets containing Lorem Ipsum passages, and
+                            more recently with desktop publishing software like
+                            Aldus PageMaker including versions of Lorem Ipsum
+                            Lorem Ipsum is simply dummy text of the printing and
+                            typesetting industry. Lorem Ipsum has been the
+                            industry's standard dummy text ever since the 1500s,
+                            when an unknown printer took a galley of type and
+                            scrambled it to make a type specimen book. It has
+                            survived not only five centuries, but also the leap
+                            into electronic typesetting, remaining essentially
+                            unchanged. It was popularised in the 1960s with the
+                            release of Letraset sheets containing Lorem Ipsum
+                            passages, and more recently with desktop publishing
+                            software like Aldus PageMaker including versions of
+                            Lorem Ipsum Lorem Ipsum is simply dummy text of the
+                            printing and typesetting industry. Lorem Ipsum has
+                            been the industry's standard dummy text ever since
+                            the 1500s, when an unknown printer took a galley of
+                            type and scrambled it to make a type specimen book.
+                            It has survived not only five centuries, but also
+                            the leap into electronic typesetting, remaining
+                            essentially unchanged. It was popularised in the
+                            1960s with the release of Letraset sheets containing
+                            Lorem Ipsum passages, and more recently with desktop
+                            publishing software like Aldus PageMaker including
+                            versions of Lorem Ipsum Lorem Ipsum is simply dummy
+                            text of the printing and typesetting industry. Lorem
+                            Ipsum has been the industry's standard dummy text
+                            ever since the 1500s, when an unknown printer took a
+                            galley of type and scrambled it to make a type
+                            specimen book. It has survived not only five
+                            centuries, but also the leap into electronic
+                            typesetting, remaining essentially unchanged. It was
+                            popularised in the 1960s with the release of
+                            Letraset sheets containing Lorem Ipsum passages, and
+                            more recently with desktop publishing software like
+                            Aldus PageMaker including versions of Lorem Ipsum
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="dropdown">
-    <div @click="toggleDropdown" class="dropdown-toggle">
-      History
     </div>
-    <div v-if="isOpen" class="dropdown-content">
-      <div class="scrollable-area">
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
-      </div>
-    </div>
-  </div>
-       
-      </div>
-      </div>
-    
-  </template>
-  
-  <script setup lang="ts">
- 
-  
- const currentDate = computed(() => {
+</template>
+
+<script setup lang="ts">
+const currentDate = computed(() => {
     const today = new Date();
-    return today.toLocaleDateString(undefined, { year: 'numeric', month: 'long', day: 'numeric' });
-  });
- 
+    return today.toLocaleDateString(undefined, {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    });
+});
 
-
-  const skill = "idk";
- const isOpen = ref(false);
+const skill = "idk";
+const isOpen = ref(false);
 
 const toggleDropdown = () => {
-  isOpen.value = !isOpen.value;
+    isOpen.value = !isOpen.value;
 };
-  const summary = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
- 
+const summary =
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+</script>
 
-  </script>
-  
-  <style scoped lang="scss">
-  @use "~/assets/scss/summary.scss";
-  </style>
-  
-  
+<style scoped lang="scss">
+@use "~/assets/scss/summary.scss";
+</style>
--- pages/dashboard/journal.vue
+++ pages/dashboard/journal.vue
@@ -4,51 +4,85 @@
             <div class="journal-history">
                 <div id="recent">Recent</div>
                 <div class="searchbar">
-                    <input type="date" v-model="selectedDate" @input="onDateChange" class="date-input" placeholder="Select a date"/>
+                    <input
+                        type="date"
+                        v-model="selectedDate"
+                        @input="onDateChange"
+                        class="date-input"
+                        placeholder="Select a date"
+                    />
                 </div>
-                    <ul class="reflection-list" v-if="filteredReflections?.length" v-for="reflection in filteredReflections">
-                        <li class="reflection-item" onclick="window.location.href='/dashboard/reflection'">
-                            <p style="font-size: smaller">{{ new Date(reflection.date).getDate() }}-{{ new Date(reflection.date).getMonth() + 1}}-{{ new Date(reflection.date).getFullYear() }}</p>
-                            <div>{{ reflection.summary }}</div>
-                        </li>
-                    </ul> 
-                    <div v-else style="display: flex; justify-content: center; align-items: center; height: 100px;">No reflections found.</div>
-                    <div style="display: flex; justify-content: center; align-items: center;padding:0.5rem" >
-                    </div>
+                <ul
+                    class="reflection-list"
+                    v-if="filteredReflections?.length"
+                    v-for="reflection in filteredReflections"
+                >
+                    <li
+                        class="reflection-item"
+                        onclick="window.location.href='/dashboard/reflection'"
+                    >
+                        <p style="font-size: smaller">
+                            {{ new Date(reflection.date).getDate() }}-{{
+                                new Date(reflection.date).getMonth() + 1
+                            }}-{{ new Date(reflection.date).getFullYear() }}
+                        </p>
+                        <div>{{ reflection.summary }}</div>
+                    </li>
+                </ul>
+                <div
+                    v-else
+                    style="
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                        height: 100px;
+                    "
+                >
+                    No reflections found.
+                </div>
+                <div
+                    style="
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                        padding: 0.5rem;
+                    "
+                ></div>
             </div>
-            <div class="chat-loaded">
-                Right panel
-            </div>
+            <div class="chat-loaded"><Summary /></div>
         </div>
     </NuxtLayout>
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from 'vue';
+import { ref, computed } from "vue";
 
 // Fetching reflections from API
 const { data: reflections } = await useFetch("/api/reflections/user");
 
-const layout = "board"
-const selectedDate = ref('')
+const layout = "board";
+const selectedDate = ref("");
 
 // Function to filter reflections based on selected date
 const filteredReflections = computed(() => {
-    if (!selectedDate.value) return reflections.value || []
+    if (!selectedDate.value) return reflections.value || [];
 
-    const targetDate = new Date(selectedDate.value).toISOString().split('T')[0]
-    return reflections.value?.filter(reflection => {
-        const reflectionDate = new Date(reflection.date).toISOString().split('T')[0]
-        return reflectionDate === targetDate;
-    }) || []
-})
+    const targetDate = new Date(selectedDate.value).toISOString().split("T")[0];
+    return (
+        reflections.value?.filter((reflection) => {
+            const reflectionDate = new Date(reflection.date)
+                .toISOString()
+                .split("T")[0];
+            return reflectionDate === targetDate;
+        }) || []
+    );
+});
 
 // Function triggered on date input change
 function onDateChange(event: Event) {
     const target = event.target as HTMLInputElement;
     selectedDate.value = target.value;
 }
-
 </script>
 
 <style scoped lang="scss">
