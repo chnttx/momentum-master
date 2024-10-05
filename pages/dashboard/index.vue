6e5a9288e5802dec0c8a1276d6d23092becc463a
cyy020811 <jimyang@gmail.com>
2024-10-05 22:52:50 +1000

    feat: creat dashboard page basic structure

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,7 +1,20 @@
 <!-- Landing page -->
 <template>
     <NuxtLayout :name="layout">
-        <NuxtPage />
+        <div id="dashboard-container">
+            <div id="dashboard-left" class="dashboard-content">
+                <div class="dashboard-box dashboard-left-box"></div>
+                <div class="dashboard-box dashboard-left-box"></div>
+                <div class="dashboard-box dashboard-left-box"></div>
+            </div>
+            <div id="dashboard-right" class="dashboard-content">
+                <div
+                    id="calendar"
+                    class="dashboard-box dashboard-right-box"
+                ></div>
+                <div id="quote" class="dashboard-box dashboard-right-box"></div>
+            </div>
+        </div>
     </NuxtLayout>
 </template>
 
@@ -9,4 +22,6 @@
 const layout = "board";
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+@use "~/assets/scss/dashboard.scss";
+</style>
