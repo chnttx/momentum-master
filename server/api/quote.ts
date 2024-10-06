784fe9d76494f5b526daa5e4ae4d0e7b985925ea
cyy020811 <jimyang@gmail.com>
2024-10-07 00:03:46 +1100

    feat: show user skill proficiency

--- server/api/quote.ts
+++ server/api/quote.ts
@@ -1,5 +1,7 @@
 export default defineEventHandler(async (event) => {
-    const response = await $fetch("https://zenquotes.io/api/random");
+    const response: Array<any> = await $fetch(
+        "https://zenquotes.io/api/random"
+    );
 
     return {
         quote: response[0].q,
